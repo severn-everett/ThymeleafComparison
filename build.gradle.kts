820573f2de3f3449ba1bd7eb72962ec5e7edbd42
Severn Everett <severn.everett@gmail.com>
2023-01-30 08:12:34 +0100

    Upgraded Spring Boot to 3.0.2

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,7 +2,7 @@ import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
     kotlin("jvm") version "1.8.0"
-    id("org.springframework.boot") version "3.0.1"
+    id("org.springframework.boot") version "3.0.2"
     id("io.spring.dependency-management") version "1.1.0"
 }
 
